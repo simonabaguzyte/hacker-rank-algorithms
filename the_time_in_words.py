fc42bb2aac7f2653760b0faf2ace1aa90696c68d
Simona <baguzyte98@gmail.com>
2022-07-18 19:49:28 +0300

    An update

--- the_time_in_words.py
+++ the_time_in_words.py
@@ -1,11 +1,7 @@
-import math
 import os
-import random
-import re
-import sys
 
 def timeInWords(h, m):
-    numbers = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen', 'twenty', 'twenty one', 'twenty two', 'twenty three', 'twenty four', 'twenty five', 'twenty six', 'twenty seven', 'twenty eight', 'twenty nine', 'thirty','thirty one', 'thirty two', 'thirty three', 'thirty four', 'thirty five', 'thirty six', 'thirty seven', 'thirty eight', 'thirty nine', 'forty', 'forty one', 'forty two', 'forty three', 'forty four']
+    numbers = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen', 'twenty', 'twenty one', 'twenty two', 'twenty three', 'twenty four', 'twenty five', 'twenty six', 'twenty seven', 'twenty eight', 'twenty nine', 'thirty', 'thirty one', 'thirty two', 'thirty three', 'thirty four', 'thirty five', 'thirty six', 'thirty seven', 'thirty eight', 'thirty nine', 'forty', 'forty one', 'forty two', 'forty three', 'forty four']
 
     converted_into_words = ''
 
@@ -28,19 +24,15 @@ def timeInWords(h, m):
             converted_into_words = converted_into_words + numbers[h + 1] + " o' clock"
         elif m < 30:
             converted_into_words = converted_into_words + numbers[m] + " " + minute + " past " + numbers[h]
-        elif m > 30 and m < 45:
-            converted_into_words = converted_into_words + numbers[m] + " " + minute + " past " + numbers[h]
-        elif m > 45:
+        elif m > 30:
             converted_into_words = converted_into_words + numbers[60 - m] + " " + minute + " to " + numbers[h + 1]
 
     return converted_into_words
 
+    
 if __name__ == '__main__':
-
-    h = int(input("\n\nEnter the hours portion: ").strip())
-
-    m = int(input("\nEnter the minutes portion: ").strip())
+    h = int(input().strip())
+    m = int(input().strip())
 
     result = timeInWords(h, m)
-
     print(result)
